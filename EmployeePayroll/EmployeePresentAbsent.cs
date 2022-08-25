55c4887f1dabb952478c15dbbcf1ba2b05a0ce7a
29payal <sharma29payal29@gmail.com>
2022-08-25 16:04:51 +0530

    [Payal] UC1-check employee present or absent

---
+++ EmployeePayroll/EmployeePresentAbsent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeePayroll
+{
+    internal class EmployeePresentAbsent
+    {
+        public static void EmployeeAttenanceCheck()
+        {
+            Random random = new Random();
+            int empCheck=random.Next(0, 2);//0,1
+            Console.WriteLine("Random generated value "+ empCheck);
+            if(empCheck==0)
+            {
+                Console.WriteLine("Employee Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee Absent");
+            }
+        }
+    }
+}
--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -8,6 +8,29 @@ namespace EmployeePayroll
         {
 
             Console.WriteLine("Welcome to Employee wage");
+            //if(condition)
+            //{
+
+            //}
+            //else
+            //{
+
+            //}
+            // Random random = new Random();
+            //Console.WriteLine(Random.Next(10));//0-9
+            // Console.WriteLine(random.Next(10,20));//10-19
+            //Console.WriteLine(random.NextDouble());//0.0-1.0
+            Console.WriteLine("Choose option\n 1. Check absent present");
+            int option=Convert.ToInt32(Console.ReadLine());
+             switch(option)
+            {             
+                case 1:EmployeePresentAbsent.EmployeeAttenanceCheck();
+                       break;
+                default:
+                    Console.WriteLine("Entered wrong choice");
+                    break;
+
+             }
         }
     }
 }
