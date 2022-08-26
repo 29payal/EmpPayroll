1e3bda43435e35c0a5ed5f1256abab8002ff0ed5
29payal <111033408+29payal@users.noreply.github.com>
2022-08-26 17:15:40 +0530

    Update Program.cs

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -8,18 +8,6 @@ namespace EmployeePayroll
         {
 
             Console.WriteLine("Welcome to Employee wage");
-            //if(condition)
-            //{
-
-            //}
-            //else
-            //{
-
-            //}
-            // Random random = new Random();
-            //Console.WriteLine(Random.Next(10));//0-9
-            // Console.WriteLine(random.Next(10,20));//10-19
-            //Console.WriteLine(random.NextDouble());//0.0-1.0
             Console.WriteLine("Choose option\n 1. Check absent present\t 2. Daily emp wage");
             int option=Convert.ToInt32(Console.ReadLine());
              switch(option)
