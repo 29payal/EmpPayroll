e4472b2ab9b87c26be0dd515eaf902bebb8d6e24
Payal Sharma <https://github.com/29payal>
2022-09-05 18:07:04 +0530

    [Payal] UC7-Refactor

--- EmployeePayroll/HrsOrDaysForAMonth.cs
+++ EmployeePayroll/HrsOrDaysForAMonth.cs
@@ -9,16 +9,16 @@ namespace EmployeePayroll
     internal class HrsOrDaysForAMonth
 
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_Of_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+
         public static void CalculateEmployeeWage()
-        {
 
-            const int IS_FULL_TIME = 1;
-            const int IS_PART_TIME = 2;
-            const int EMP_RATE_PER_HOUR = 20;
-            const int NUM_Of_WORKING_DAYS = 20;
-            const int MAX_HRS_IN_MONTH = 100;
-            {
-
+              {
                 // Variables
                 int empHrs = 0;
                 int empWage = 0;
@@ -30,34 +30,30 @@ namespace EmployeePayroll
                 {
                     totalWorkingDays++;
                     Random random = new Random();
-                    int empCheck = random.Next(0, 3);
+                    int empCheck = random.Next(1, 3);//1,2
                     switch (empCheck)
                     {
                         case IS_FULL_TIME:
                             empHrs = 8;
-                            Console.WriteLine("Employee is Full Time");
+                            
                             break;
 
                         case IS_PART_TIME:
                             empHrs = 4;
-                            Console.WriteLine("Employee is Present for Part Time");
+                            
                             break;
 
                         default:
                             empHrs = 0;
-                            Console.WriteLine("Employee is Absent");
+                            
                             break;
                     }
                     totalEmpHrs += empHrs;
-                    Console.WriteLine("Days:" + totalWorkingDays + "Employee Hours: " + empHrs);
+                    Console.WriteLine("Days {0} and  empHrs {1}" ,totalWorkingDays ,totalEmpHrs);
                 }
                 totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine();
                 Console.WriteLine("Total Employee Wage is: " + totalEmpWage);
-
-            }
-
-
-        }
-
-    }
+}
+}
 }
