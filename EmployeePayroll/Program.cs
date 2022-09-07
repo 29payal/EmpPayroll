6e9b415ebd5c638f1bc1aa756aa1f86355cb3766
Payal Sharma <https://github.com/29payal>
2022-09-07 13:06:50 +0530

    [payal UC7-Refactor

--- EmployeePayroll/Program.cs
+++ EmployeePayroll/Program.cs
@@ -8,7 +8,7 @@ namespace EmployeePayroll
         {
 
             Console.WriteLine("Welcome to Employee wage");
-            Console.WriteLine("Choose option\n 1. Check absent present\t 2. Daily emp wage\t 3.Part time Wages\t 4.Parttime Wage SwitchCase\t 5. Emp wage for month\t 6. Num of working days or totalworkinghrs for a month ");
+            Console.WriteLine("Choose option\n 1. Check absent present\t 2. Daily emp wage\t 3.Part time Wages\t 4.Parttime Wage SwitchCase\t 5. Emp wage for month\t 6. Num of working days or totalworkinghrs for a month\t 7.Refactor ");
             int option=Convert.ToInt32(Console.ReadLine());
              switch(option)
             {             
@@ -29,6 +29,9 @@ namespace EmployeePayroll
                     break;
                 case 6:
                     HrsOrDaysForAMonth.CalculateEmployeeWage();
+                    break;
+                case 7:
+                    Refactor.CalculateEmployeeWage();
                     break;
                 default:
                     Console.WriteLine("Entered wrong choice");
--- EmployeePayroll/Refactor.cs
+++ EmployeePayroll/Refactor.cs
@@ -8,5 +8,55 @@ namespace EmployeePayroll
 {
     internal class Refactor
     {
+        public const int IS_FULL_TIME = 2;
+        public const int IS_PART_TIME = 1;
+        public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_Of_WORKING_DAYS = 20;
+        public const int MAX_HRS_IN_MONTH = 100;
+
+
+        public static void CalculateEmployeeWage()
+
+        {
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
+            int totalEmpWage = 0;
+            int totalEmpHrs = 0;
+            int totalWorkingDays = 0;
+            //computation
+            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_Of_WORKING_DAYS)
+            {
+                totalWorkingDays++;
+                Random random = new Random();
+                int empCheck = random.Next(1, 3);
+                switch (empCheck)
+                {
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        Console.WriteLine("Employee is Full Time");
+                        break;
+
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        Console.WriteLine("Employee is Present for Part Time");
+                        break;
+
+                    default:
+                        empHrs = 0;
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                }
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Days:" + totalWorkingDays + "Employee Hours: " + empHrs);
+            }
+            totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Employee Wage is: " + totalEmpWage);
+            return;
+        }
+        public void Main(string[] args)
+        {
+            CalculateEmployeeWage();
+        }
     }
 }
